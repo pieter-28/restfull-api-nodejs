f2a9b54f6850113900c370f2070126fb42ebbb90
pieter-28 <peterlase28@gmail.com>
2025-08-29 09:58:00 +0700

    Create res api crud user serta testing nya

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -22,7 +22,44 @@ const login = async (req, res, next) => {
     }
 }
 
+const get = async (req, res, next) => {
+    try {
+        const username = req.user.username;
+        const result = await userService.get(username);
+        res.status(200).json({
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+const update = async (req, res, next) => {
+    try {
+        const username = req.user.username;
+        const request = req.body;
+        request.username = username;
+
+        const result = await userService.update(request);
+        res.status(200).json({
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+const logout = async (req, res, next) => {
+    try {
+        await userService.logout(req.user.username);
+        res.status(200).json({
+            data: "logout success"
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
 export default {
-    register,
-    login
+    register, login, get, update, logout
 }
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,17 +1,12 @@
 import {validate} from "../validation/validation.js";
 
 import {
-    getUserValidation,
-    loginUserValidation,
-    registerUserValidation,
-    updateUserValidation
+    getUserValidation, loginUserValidation, registerUserValidation, updateUserValidation
 } from "../validation/user-validation.js";
 import {prismaClient} from "../application/database.js";
 import {ResponseError} from "../error/response-error.js";
 import bcrypt from "bcrypt";
 import {v4 as uuid} from "uuid";
-import {request} from "express";
-
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request);
@@ -29,10 +24,8 @@ const register = async (request) => {
     user.password = await bcrypt.hash(user.password, 10);
 
     return prismaClient.user.create({
-        data: user,
-        select: {
-            username: true,
-            name: true
+        data: user, select: {
+            username: true, name: true
         }
     });
 }
@@ -42,10 +35,8 @@ const login = async (request) => {
     const user = await prismaClient.user.findUnique({
         where: {
             username: loginRequest.username
-        },
-        select: {
-            username: true,
-            password: true
+        }, select: {
+            username: true, password: true
         }
     });
 
@@ -62,17 +53,85 @@ const login = async (request) => {
     return prismaClient.user.update({
         data: {
             token: token
-        },
-        where: {
+        }, where: {
             username: user.username
-        },
-        select: {
+        }, select: {
             token: true
         }
     });
 }
 
+const get = async (username) => {
+    username = validate(getUserValidation, username);
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        }, select: {
+            username: true, name: true
+        }
+    });
+
+    if (!user) {
+        throw new ResponseError(404, "user is not found");
+    }
+
+    return user;
+}
+
+const update = async (request) => {
+    const user = validate(updateUserValidation, request);
+
+    const totalUserInDatabase = await prismaClient.user.count({
+        where: {
+            username: user.username
+        }
+    });
+
+    if (totalUserInDatabase !== 1) {
+        throw new ResponseError(404, "user is not found");
+    }
+
+    const data = {};
+    if (user.name) {
+        data.name = user.name;
+    }
+    if (user.password) {
+        data.password = await bcrypt.hash(user.password, 10);
+    }
+
+    return prismaClient.user.update({
+        where: {
+            username: user.username
+        }, data: data, select: {
+            username: true, name: true
+        }
+    });
+}
+
+const logout = async (username) => {
+    username = validate(getUserValidation, username);
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        }
+    });
+
+    if (!user) {
+        throw new ResponseError(404, "user is not found");
+    }
+
+    return prismaClient.user.update({
+        where: {
+            username: user.username
+        }, data: {
+            token: null
+        }, select: {
+            username: true
+        }
+    });
+}
+
 export default {
-    register,
-    login
+    register, login, get, update, logout
 }
